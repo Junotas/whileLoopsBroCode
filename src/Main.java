b858b2377daef2870cf90e0f3f5665b7aa200616
Ludvig <ludvig.fendert@appliedtechnology.se>
2024-05-02 14:51:40 +0200

    Second

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //while loop exe block if the condition is true
+        String welcome ="Welcome ";
+
+//open scanner and ask for name, defined name as the string the user inputs
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = in.nextLine();
+
+        while(name.isBlank()){
+
+            System.out.println("Enter your name: ");
+
+            name = in.nextLine();
+
+        }
+        System.out.println("Welcome "+name+"!");
+
+
     }
 }
